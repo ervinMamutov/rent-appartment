56a4906c437bedc8580bb6a4f5e7d7991cbe9ec1
Ervin Mamutov <ervin.news@gmail.com>
2024-07-27 18:10:42 +0200

    added mongoDB settings

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -9,12 +9,12 @@ const HomeProperties = () => {
 
   return (
     <>
-      <section classNameName='px-4 py-6'>
-        <div classNameName='container-xl lg:container m-auto'>
-          <h2 classNameName='text-3xl font-bold text-blue-500 mb-6 text-center'>
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto'>
+          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
             Recent Properties
           </h2>
-          <div classNameName='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {recentProperties === 0 ? (
               <p>No Properties Found</p>
             ) : (
