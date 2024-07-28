2f2763565450395add2e754ce3a61d46ab6da922
Ervin Mamutov <ervin.news@gmail.com>
2024-07-28 13:40:32 +0200

    modified: added Fetch single property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,34 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
+
 const PropertyPage = () => {
+  const { id } = useParams();
+  console.log({ id: id });
+
+  const [property, setProperty] = useState(null);
+  console.log({ property: property });
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Error fetching property:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
   return <div>PropertyPage</div>;
 };
 
