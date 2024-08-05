32a058b2f13350c946191b7f5792c8af36046127
Ervin Mamutov <ervin.news@gmail.com>
2024-08-05 12:49:50 +0200

    addede get user and db acsses

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,8 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/utils/authOptions';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -17,6 +20,16 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is require', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
 
     // Access all values from amenities and images
@@ -50,14 +63,20 @@ export const POST = async (request) => {
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
       },
-      images,
+      owner: userId,
+      // images,
     };
 
-    console.log(propertyData);
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
 
-    return new Response(
-      JSON.stringify({ message: 'Success' }, { status: 200 })
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
     );
+
+    // return new Response(
+    //   JSON.stringify({ message: 'Success' }, { status: 200 })
+    // );
   } catch (error) {
     return new Response('Failed to add property', { status: 500 });
   }
